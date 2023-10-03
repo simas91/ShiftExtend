7a28553b46eed1c9e7f971d47eeda6c9f4abf998
simas91 <simas91@gmail.com>
2023-10-03 12:47:32 +0100

    Add Bootstrapper to WPF
    Wired bootstrapper for caliburn.micro in wpf. Changed launch window

---
+++ SEDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using SEDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SEDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        // Sets the launch window
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
