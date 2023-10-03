ccfa54ddc5c9e8e7db046c6c8be3544c765a5e2d
simas91 <simas91@gmail.com>
2023-10-03 10:54:29 +0100

    Add Web Api
    Added asp.net core web app blank project for web api. Changed default database name

---
+++ SEApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
