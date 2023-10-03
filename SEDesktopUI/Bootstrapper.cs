afa2e2fe590c96876a1a536f11b3fa85095ac55e
simas91 <simas91@gmail.com>
2023-10-03 12:59:52 +0100

    Add caliburn micro to WPF
    Added caliburn micro and configured starup auto wiring for views and viewModels

--- SEDesktopUI/Bootstrapper.cs
+++ SEDesktopUI/Bootstrapper.cs
@@ -11,15 +11,53 @@ namespace SEDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // Dependency Injection with SimpleContainer, initializes new instances
+        private readonly SimpleContainer _container = new();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            // SINGLETON
+            // brings extra stuff from caliburn.micro
+            // makes that class to be used for entire duration of the app, instead of making new one on request
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // wires up all views with viewModels on startup
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         // Sets the launch window
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        // wires up view with viewModel
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
