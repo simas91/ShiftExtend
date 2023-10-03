41eb3a84050fa7d91ec4ee79644edd0e7cbd8e9d
simas91 <simas91@gmail.com>
2023-10-03 14:02:52 +0100

    Add Database Tables
    Added Tables for users and overtimes

---
+++ SEData/dbo/Tables/Overtimes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Overtimes] (
+    [Id]           INT      IDENTITY (1, 1) NOT NULL,
+    [OvertimeDate] DATE     NOT NULL,
+    [StartTime]    TIME (7) NOT NULL,
+    [EndTime]      TIME (7) NOT NULL,
+    [OP]           BIT      DEFAULT ((0)) NOT NULL,
+    [CB]           BIT      DEFAULT ((0)) NOT NULL,
+    [RT]           BIT      DEFAULT ((0)) NOT NULL,
+    [Pack]         BIT      DEFAULT ((0)) NOT NULL
+);
---
+++ SEData/dbo/Tables/User.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [AuthUserId]    NVARCHAR (450) NOT NULL,
+    [FirstName]     NVARCHAR (50)  NOT NULL,
+    [LastName]      NVARCHAR (50)  NOT NULL,
+    [Employer]      NVARCHAR (10)  NOT NULL,
+    [OP]            BIT            DEFAULT ((0)) NOT NULL,
+    [CB]            BIT            DEFAULT ((0)) NOT NULL,
+    [RT]            BIT            DEFAULT ((0)) NOT NULL,
+    [Pack]          BIT            DEFAULT ((0)) NOT NULL,
+    [FirstAider]    BIT            DEFAULT ((0)) NOT NULL,
+    [FireWarden]    BIT            DEFAULT ((0)) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+);
